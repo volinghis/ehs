8d332f3bb5546bec6cd08d067161d0f3c69620f1
qjj <584419234@qq.com>
2019-12-30 15:47:48 +0800

    备件台账

---
+++ src/main/java/com/ehs/eam/eamPartLibraryManager/entity/EamPartLibrary.java
@@ -0,0 +1,27 @@
+/**   
+ * Copyright © 2019 西安东恒鑫源软件开发有限公司版权所有.
+ * 
+ * 功能描述：
+ * @Package: com.ehs.eam.eamPartLibraryManager.entity 
+ * @author: qjj   
+ * @date: 2019年12月30日 下午3:47:29 
+ */
+package com.ehs.eam.eamPartLibraryManager.entity;
+
+/**   
+* Copyright: Copyright (c) 2019 西安东恒鑫源软件开发有限公司
+* @ClassName: EamPartLibrary.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: qjj
+* @date: 2019年12月30日 下午3:47:29 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2019年12月30日     qjj           v1.0.0               修改原因
+*/
+public class EamPartLibrary {
+
+}
