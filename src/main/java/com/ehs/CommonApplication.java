7ca9d508bce9bdee1ff272a97c7bc31e0384e995
selfvolinghis <2629106344@qq.com>
2020-01-03 18:58:06 +0800

    更改基本配置

--- src/main/java/com/ehs/CommonApplication.java
+++ src/main/java/com/ehs/CommonApplication.java
@@ -2,10 +2,11 @@ package com.ehs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableCaching
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class CommonApplication {
 
